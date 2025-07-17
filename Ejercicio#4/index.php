e2186a06c5d1f79f5cca08b7a2e05c6c89fddfef
Samuel Pacheco <142636362+SamsPacheco@users.noreply.github.com>
2025-07-16 19:09:24 -0600

    ajuste ejercicio 4

--- Ejercicio#4/index.php
+++ Ejercicio#4/index.php
@@ -4,7 +4,7 @@ $array_numeros = array(2,4,5);
 
 function sumaPares ($array_valores){
     $suma = 0;
-    for($i = 0; $i < 5; $i++){
+    for($i = 0; $i < count($array_valores); $i++){
         if($array_valores[$i] % 2 == 0){
             $suma += $array_valores[$i];
         }
